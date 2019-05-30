7f9505d4d273edfc2a7b43a53e9417bb08fc0f79
jSandersWhite <j.sanders.white@gmail.com>
2019-05-29 23:02:58 -0400

    Update routes

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('APP_NAME', 'jswhite'),
 
     /*
     |--------------------------------------------------------------------------
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'http://www.jsanderswhite.dev.com'),
 
     'asset_url' => env('ASSET_URL', null),
 
