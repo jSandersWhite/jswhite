994ee196cf1f0ce6776c2b7f312db0e424594280
jSandersWhite <j.sanders.white@gmail.com>
2019-05-29 23:39:51 -0400

    Create foundation

---
+++ resources/views/about.blade.php
@@ -0,0 +1,20 @@
+@extends('layout')
+
+@section('content')
+<h2>About me</h2>
+<p>My name is Joshua White. I'm a full-stack developer in South Carolina.</p>
+<p>I graduated from Furman University in 2019 with a B.L.A. in Information Technology.</p>
+<p>This is a content placeholder!</p>
+<h3>Hobbies include:</h3>
+<ul>
+    <li>Tabletop gaming</li>
+    <ul>
+        <li>Warmachine</li>
+        <li>Guild Ball</li>
+        <li>Magic: the Gathering</li>
+        <li>Board games</li>
+    </ul>
+    <li>Video games</li>
+    <li>Soccer</li>
+</ul>
+@endsection
---
+++ resources/views/contact.blade.php
@@ -0,0 +1,6 @@
+@extends('layout')
+
+@section('content')
+<p>Email or call me for more information</p>
+
+@endsection
